34382494eeff4e5e6bc7eb6c8a1f29a40124fdfd
chenbj <chenbj@wedoctor.com>
2022-01-19 20:38:33 +0800

    spring cloud gateway网关路由demo示例

--- src/main/java/com/abinge/boot/staging/StagingApplication.java
+++ src/main/java/com/abinge/boot/staging/StagingApplication.java
@@ -2,17 +2,15 @@ package com.abinge.boot.staging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
- * @package com.abinge.boot.staging
- * @description 脚手架工程
  * @author abinge
- * @date 2019/1/5 15:45
  * @version 1.0.0
  * <p>Copyright Copyright (c) 2019/1/5</p>
+ * @package com.abinge.boot.staging
+ * @description 脚手架工程
+ * @date 2019/1/5 15:45
  */
-@EnableJpaAuditing
 @SpringBootApplication
 public class StagingApplication {
 
